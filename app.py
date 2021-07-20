dd454ba73330ed8e772b7291c1455d98e905c597
dekapaan <54145779+dekapaan@users.noreply.github.com>
2021-07-20 15:53:09 +0200

    Modify to show endpoint data

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     data = requests.get(api_url).json()
 
-    return "<strong>Random joke: </strong> {}".format(data['value'])
+    return "<img src='{}'></img></br> <strong>Endpoint data from Chuck Norris API: </strong></br>{}".format(data['icon_url'],
+                                                                                                      data)
 
 if __name__ == '__main__':
     app.run(debug=True)
