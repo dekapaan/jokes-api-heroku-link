3894d3f12f8025dd1a865905d73d16bb581f1eea
dekapaan <54145779+dekapaan@users.noreply.github.com>
2021-07-21 14:19:39 +0200

    Added route

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     data = requests.get(api_url).json()
 
-    return "<img src='{}'></img></br> <strong>Endpoint data from Chuck Norris API: </strong></br>{}".format(data['icon_url'],
-                                                                                                      data)
+    return "<img src='{}'></img></br> <strong>Endpoint data from Chuck Norris API: </strong></br>{}".format(
+        data['icon_url'],data)
+
+
+@app.route('/categories', methods=["GET"])
+def get_chuck_norris_categories():
+    list = requests.get("https://api.chucknorris.io/jokes/categories")
+    return "{}".format(list.json())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
